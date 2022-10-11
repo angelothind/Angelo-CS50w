e883344a6f859d73a00d67c9d4e5c9a8b152c359
angelothind <angelo.thind@gmail.com>
2022-10-11 11:07:49 +0700

    idk

--- lecture2python/lect2pytry.py
+++
@@ -1,3 +0,0 @@
-list1 = ["Harr", "Larry", "sid"]
-
-print(list1[0][2])
--- lecture2python/mydecorators.py
+++ lecture2python/mydecorators.py
@@ -1,12 +1,1 @@
-def cubic(x):
-    def twice(x):
-        print("this is your sqaure numb",x())
-        print("This is your cubic numb", x())
-
-
-@cubic  
-def square(x):
-    return x*x
-
-square(2)
 
